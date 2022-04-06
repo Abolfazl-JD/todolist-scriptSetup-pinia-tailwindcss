69d2aed0c9d6934e02a38a075304d63f16e7d8b5
abolfazl-hue <abolfazljdst@gmail.com>
2022-04-06 14:25:21 +0430

    remove tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,72 +1,55 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
+
 import { VitePWA } from 'vite-plugin-pwa'
-import replace from '@rollup/plugin-replace'
-
-const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
-  manifest: {
-    name: 'PWA Router',
-    short_name: 'PWA Router',
-    theme_color: '#ffffff',
-    icons: [
-      {
-        src: 'pwa-192x192.png', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/pwa-512x512.png', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: 'pwa-512x512.png', // <== don't add slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
-    ],
-  },
-  devOptions: {
-    enabled: process.env.SW_DEV === 'true',
-    /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
-  },
-}
-
-const replaceOptions = { __DATE__: new Date().toISOString() }
-const claims = process.env.CLAIMS === 'true'
-const reload = process.env.RELOAD_SW === 'true'
-
-if (process.env.SW === 'true') {
-  pwaOptions.srcDir = 'src'
-  pwaOptions.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
-  pwaOptions.strategies = 'injectManifest'
-  ;(pwaOptions.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest'
-  ;(pwaOptions.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
-}
-
-if (claims)
-  pwaOptions.registerType = 'autoUpdate'
-
-if (reload) {
-  // @ts-ignore
-  replaceOptions.__RELOAD_SW__ = 'true'
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.BASE_URL || 'https://github.com/',
-  build: {
-    sourcemap: process.env.SOURCE_MAP === 'true',
-  },
   plugins: [
-    Vue(),
-    VitePWA(pwaOptions),
-    replace(replaceOptions),
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      base : '/',
+      manifest: {
+        name: 'Jalil todolist',
+        short_name: 'todolist',
+        description: 'vue vite app with new scriptSetup and pinia with tailwindcss',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // <== don't add slash, for testing
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png', // <== don't remove slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png', // <== don't add slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        sourcemap: true 
+      },
+      devOptions: {
+        enabled: process.env.SW_DEV === 'true',
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+        navigateFallback: 'index.html',
+      },
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
