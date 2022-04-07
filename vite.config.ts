4afc3c542ebae401cb683c9e2e97c46088e65e42
abolfazl-hue <abolfazljdst@gmail.com>
2022-04-07 11:25:08 +0430

    change pwa configure

--- vite.config.ts
+++ vite.config.ts
@@ -8,43 +8,39 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      base : '/',
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
       manifest: {
-        name: 'Jalil todolist',
-        short_name: 'todolist',
-        description: 'vue vite app with new scriptSetup and pinia with tailwindcss',
-        theme_color: '#ffffff',
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
         icons: [
           {
-            src: 'pwa-192x192.png', // <== don't add slash, for testing
-            sizes: '192x192',
-            type: 'image/png',
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png', // <== don't remove slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png', // <== don't add slash, for testing
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
           },
         ],
       },
-      workbox: {
-        sourcemap: true 
-      },
-      devOptions: {
-        enabled: process.env.SW_DEV === 'true',
-        /* when using generateSW the PWA plugin will switch to classic */
-        type: 'module',
-        navigateFallback: 'index.html',
-      },
-    }),
+    }),,
   ],
   resolve: {
     alias: {
